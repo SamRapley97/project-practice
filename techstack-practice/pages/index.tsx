19eb95912840743b13d7ad462e5ef769995afe81
SamRapley97 <samrapley@protonmail.com>
2022-02-16 15:05:58 +0000

    created calculator

--- techstack-practice/pages/index.tsx
+++ techstack-practice/pages/index.tsx
@@ -2,32 +2,13 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import Calculator from "./calculator";
 
-const examples = (
-  <>
-    <Calculator left={1} operator="+" right={2} />
-    <Calculator left={1} operator="-" right={2} />
-    <Calculator left={1} operator="*" right={2} />
-    <Calculator left={1} operator="/" right={2} />
-  </>
-);
-const operations = {
-  "+": (left: number, right: number): number => left + right,
-  "-": (left: number, right: number): number => left - right,
-  "*": (left: number, right: number): number => left * right,
-  "/": (left: number, right: number): number => left / right,
-};
-type CalculatorProps = {
-  left: number;
-  operator: keyof typeof operations;
-  right: number;
-};
 const Home: NextPage = () => {
   return (
     <div>
       Hello
       <br></br>
       <Link href="/about">About us</Link>
-      <Calculator />
+      <Calculator left={4} operator={"+"} right={5} />
     </div>
   );
 };
